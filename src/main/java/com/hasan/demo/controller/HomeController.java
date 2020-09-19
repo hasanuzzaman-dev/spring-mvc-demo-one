d0ad714671a411784c71dc4791512e78587d4ef1
hasanuzzman-dev <hasanuzzaman.dev@gmail.com>
2020-09-19 21:27:43 +0600

    25.use of ModelAndView

--- src/main/java/com/hasan/demo/controller/HomeController.java
+++ src/main/java/com/hasan/demo/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.hasan.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -16,12 +17,15 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int i, @RequestParam("num2") int j, HttpSession session){
+    public ModelAndView add(@RequestParam("num1") int i, @RequestParam("num2") int j){
+
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("result");
 
         int result = i + j;
 
-        session.setAttribute("result",result);
+        mv.addObject("result",result);
 
-        return "result";
+        return mv;
     }
 }
